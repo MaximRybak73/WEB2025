3983d31b083f954f34b5cf8e30bf7c054f392631
Arman <galstyanarman05@gmail.com>
2025-03-13 15:02:55 +0300

    123

---
+++ target/classes/com/example/jdbc/models/Person.kt
@@ -0,0 +1,11 @@
+package com.example.jdbc.models
+
+/**
+ * Модель пользователя. Того, который использует пиложение, а не записан в базу данных. Может дополняться полями для
+ * ссылки на карзину покупок, или что еще будет нужно в процессе работы с ним.
+ */
+class Person {
+    var login: String? = null
+    var password: String? = null
+    var id: Int? = null
+}
